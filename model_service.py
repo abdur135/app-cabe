f580055a887b01bf448e7e35a5d94aa3c837c391
abdur135 <abdulrahmanaja135@gmail.com>
2026-04-17 16:50:28 +0700

    update deploy

--- model_service.py
+++ model_service.py
@@ -1,3 +1,7 @@
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+
 import numpy as np
 import cv2
 from PIL import Image
@@ -6,11 +10,12 @@ import joblib
 from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 
-# ================= LOAD MODEL =================
+# ================= LOAD MODEL SEKALI =================
+print("🔥 Loading SVM model...")
 svm_model = joblib.load("svm_model.pkl")
 label_encoder = joblib.load("label_encoder.pkl")
 
-# ================= CNN RINGAN =================
+print("🔥 Loading CNN model...")
 cnn_model = MobileNetV2(
     weights="imagenet",
     include_top=False,
@@ -19,16 +24,18 @@ cnn_model = MobileNetV2(
     alpha=0.35
 )
 
+print("✅ Model ready!")
+
 # ================= FEATURE =================
 def get_lbp(img):
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     lbp = cv2.Laplacian(gray, cv2.CV_64F)
-    hist = np.histogram(lbp.ravel(), bins=16, range=(-255,255))[0]
+    hist = np.histogram(lbp.ravel(), bins=8, range=(-255,255))[0]
     return hist / (np.sum(hist) + 1e-7)
 
 def get_hsv(img):
     hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
-    hist = cv2.calcHist([hsv],[0,1,2],None,[4,4,4],
+    hist = cv2.calcHist([hsv],[0,1,2],None,[4,4,2],
                         [0,180,0,256,0,256]).flatten()
     return hist / (np.sum(hist) + 1e-7)
 
@@ -42,10 +49,10 @@ def predict_image(file):
     img = img.resize((160,160))
     img = np.array(img)
 
-    # CNN FEATURE
+    # CNN FEATURE (optimized)
     x = preprocess_input(img)
     x = np.expand_dims(x, axis=0)
-    cnn_feat = cnn_model.predict(x, verbose=0).flatten()
+    cnn_feat = cnn_model(x, training=False).numpy().flatten()
 
     # TEXTURE
     lbp_feat = get_lbp(img)
