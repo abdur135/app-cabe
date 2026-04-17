511d816cad9344db41ded5de5fbe672c53c27c49
abdur135 <abdulrahmanaja135@gmail.com>
2026-04-17 14:57:20 +0700

    update deploy

--- app.py
+++ app.py
@@ -1,17 +1,9 @@
-import joblib
-import numpy as np
 from flask import Flask, request, jsonify, render_template
+from model_service import predict_image
 
-from feature_extractor import extract_features
-
-# ================= APP =================
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024  # 2MB
 
-# ================= LOAD MODEL =================
-svm_model = joblib.load("svm_model.pkl")
-label_encoder = joblib.load("label_encoder.pkl")
-
-# ================= LABEL MAP =================
 label_map = {
     "healthy": "Healthy (Sehat)",
     "leaf curl": "Leaf Curl (Keriting Daun)",
@@ -20,41 +12,35 @@ label_map = {
     "yellowish": "Yellowish (Virus Kuning)"
 }
 
-# ================= ROUTE =================
 @app.route("/")
 def home():
     return render_template("index.html")
 
-# ================= PREDICT =================
 @app.route("/predict", methods=["POST"])
 def predict():
+
     if "file" not in request.files:
         return jsonify({"error": "No file uploaded"})
 
     file = request.files["file"]
 
-    try:
-        features = extract_features(file).reshape(1, -1)
+    result = predict_image(file)
 
-        probs = svm_model.predict_proba(features)[0]
-        idx = int(np.argmax(probs))
-        confidence = float(np.max(probs))
+    if "error" in result:
+        return jsonify(result)
 
-        raw_label = label_encoder.inverse_transform([idx])[0].lower()
+    label = result["label"]
+    confidence = result["confidence"]
 
-        if confidence < 0.60:
-            result = "Tidak yakin / coba gambar lain"
-        else:
-            result = label_map.get(raw_label, raw_label)
+    if confidence < 0.6:
+        final = "Tidak yakin / coba gambar lain"
+    else:
+        final = label_map.get(label, label)
 
-        return jsonify({
-            "prediction": result,
-            "confidence": confidence
-        })
+    return jsonify({
+        "prediction": final,
+        "confidence": confidence
+    })
 
-    except Exception as e:
-        return jsonify({"error": str(e)})
-
-# ================= RUN =================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000, debug=True)
+    app.run()
